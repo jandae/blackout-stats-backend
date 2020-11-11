0be93f5c0391beb363b01f5581dfce3f0234bbdb
Jandae <jandae.tabo-oy@rpsgi.com>
2020-11-11 22:56:28 +0800

    initial areas parsing 👀

--- util/json_parser.py
+++ util/json_parser.py
@@ -35,23 +35,25 @@ def formatPosts(posts):
         formatted["category"] = category if category else "NULL"
         formatted["text"] = post["postText"] if post["postText"] else "NULL"
 
-        formatted["date"] = "NULL"
-        formatted["start_time"] = "NULL"
-        formatted["end_time"] = "NULL"
-        formatted["duration"] = "NULL"
-        formatted["cause"] = "NULL"
+        # formatted["date"] = "NULL"
+        # formatted["start_time"] = "NULL"
+        # formatted["end_time"] = "NULL"
+        # formatted["duration"] = "NULL"
+        # formatted["cause"] = "NULL"
+        formatted["areas"] = "NULL"
 
         if formatted["category"] != "uncategorized":
             post_meta = parsePostMeta(post["postText"])
-            formatted["date"] = post_meta["date"] if post_meta["date"] else "NULL"
-            formatted["start_time"] = post_meta["start_datetime"] if post_meta["start_datetime"] else "NULL"
-            formatted["end_time"] = post_meta["end_datetime"] if post_meta["end_datetime"] else "NULL"
-            formatted["duration"] = post_meta["duration"] if post_meta["duration"] else "NULL"
-            formatted["cause"] = post_meta["cause"] if post_meta["cause"] else "NULL"
+            # formatted["date"] = post_meta["date"] if post_meta["date"] else "NULL"
+            # formatted["start_time"] = post_meta["start_datetime"] if post_meta["start_datetime"] else "NULL"
+            # formatted["end_time"] = post_meta["end_datetime"] if post_meta["end_datetime"] else "NULL"
+            # formatted["duration"] = post_meta["duration"] if post_meta["duration"] else "NULL"
+            # formatted["cause"] = post_meta["cause"] if post_meta["cause"] else "NULL"
+            formatted["areas"] = post_meta["areas"] if post_meta["areas"] else "NULL"
 
-        formatted["photos"] = post_images if post_images else "NULL"
-        comments = post['postComments']['comments']
-        formatted["ali_comment"] = findAliComment(comments)
+        # formatted["photos"] = post_images if post_images else "NULL"
+        # comments = post['postComments']['comments']
+        # formatted["ali_comment"] = findAliComment(comments)
 
         formatted_posts.append(formatted)
 
@@ -92,8 +94,40 @@ def findMatch(text, pattern, groups=[1]):
         return formatted.strip()
     return False
 
+def parseAreas(text):
+    keywords = ["areas affected"]
+    areas = "NULL"
+
+    for key in keywords:
+        # AREAS AFFECTED: Feeder 4
+        # Baguio City: Victoria Village, Quezon Hill Proper, ...
+        title_pattern3 = re.search(
+            "^"+key+"\s*:\s*(\s*(\d\d*\.*\s*)*feeder\s*\d\d*$)(\s*(.*)\s*:\s*(.*)$)*",
+            text,
+            flags = re.IGNORECASE|re.MULTILINE
+        )
+
+
+        if title_pattern3:
+            areas = title_pattern3.group().split('\n')
+            print(areas)
+            break
+        else:
+            # AREAS AFFECTED: Dacap, Nagawa - Ampucao Itogon
+            title_pattern1 = re.search(
+                "^"+key+"\s*:\s*(.*\s*:\s*)*(\w.*)$",
+                text,
+                flags = re.IGNORECASE|re.MULTILINE
+            )
+            if title_pattern1:
+                areas = title_pattern1.group(2).split(',')
+            break
+
+
+    return areas
+
 def parseCause(text):
-    keywords = ["cause", "purpose"]
+    keywords = ["cause", "purpose", "findings"]
     cause = "NULL"
 
     for key in keywords:
@@ -105,7 +139,6 @@ def parseCause(text):
 
         if title_pattern:
             cause = title_pattern.group(1).strip()
-            print(cause)
             break
 
     return cause
@@ -114,6 +147,7 @@ def parseCause(text):
 def parsePostMeta(text):
 
     cause = parseCause(text)
+    areas = parseAreas(text)
     date = ""
     start_datetime = ""
     end_datetime = ""
@@ -234,7 +268,8 @@ def parsePostMeta(text):
                 "start_datetime": start_datetime,
                 "end_datetime": end_datetime,
                 "duration": timeDiff(start_datetime, end_datetime),
-                "cause": cause
+                "cause": cause,
+                "areas": areas
             }
 
     print("XXXXXXXX")
@@ -308,7 +343,8 @@ def parsePostMeta(text):
         "start_datetime": start_datetime,
         "end_datetime": end_datetime,
         "duration": timeDiff(start_datetime, end_datetime),
-        "cause": cause
+        "cause": cause,
+        "areas": areas
     }
 
 def parsePostMeta1(text):
